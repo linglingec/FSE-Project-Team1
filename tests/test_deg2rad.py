ee93c762a9296db012d9fdf005dc44fd245e2608
Alex Ganibaev <acleriot@gmail.com>
2022-10-30 16:30:16 +0300

    Refactor test_deg2rad

--- tests/test_deg2rad.py
+++ tests/test_deg2rad.py
@@ -3,30 +3,34 @@ from subprocess import Popen, PIPE
 import math
 
 
-def executor(x: str) -> (float, float):
-    with Popen(["./deg2rad.exe", x], stdout=PIPE) as p:
+def deg2rad_cpp(x: float) -> (float):
+    with Popen(["./deg2rad.exe", str(x)], stdout=PIPE) as p:
         output = float(p.stdout.read().decode("utf-8"))
-        expected_output = math.radians(float(x))
-    return expected_output, output
+    return output
 
 
 class Test_deg2rad(unittest.TestCase):
     def test_valid_1(self):
-        expected_output, output = executor("10")
+        expected_output = math.radians(10)
+        output = deg2rad_cpp(10)
         self.assertAlmostEqual(expected_output, output, 3)
 
     def test_valid_2(self):
-        expected_output, output = executor("57.2958")
+        expected_output = math.radians(57.2958)
+        output = deg2rad_cpp(57.2958)
         self.assertAlmostEqual(expected_output, output, 3)
 
     def test_valid_3(self):
-        expected_output, output = executor("-45")
+        expected_output = math.radians(-45)
+        output = deg2rad_cpp(-45)
         self.assertAlmostEqual(expected_output, output, 3)
 
     def test_valid_4(self):
-        expected_output, output = executor("0")
+        expected_output = math.radians(0)
+        output = deg2rad_cpp(0)
         self.assertAlmostEqual(expected_output, output, 3)
 
     def test_valid_5(self):
-        expected_output, output = executor("10000")
+        expected_output = math.radians(10000)
+        output = deg2rad_cpp(10000)
         self.assertAlmostEqual(expected_output, output, 3)
