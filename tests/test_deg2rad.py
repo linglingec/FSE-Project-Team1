bbd59e967310f4f8ca56af2c5f1cf9697c3b3b3a
aayugay99 <sasha.yugay1999@mail.ru>
2022-10-30 15:37:02 +0300

    test_deg2rad.py optimized

--- tests/test_deg2rad.py
+++ tests/test_deg2rad.py
@@ -3,33 +3,30 @@ from subprocess import Popen, PIPE
 import math
 
 
+def executor(x: str) -> (float, float):
+    with Popen(["./deg2rad.exe", x], stdout=PIPE) as p:
+        output = float(p.stdout.read().decode("utf-8"))
+        expected_output = math.radians(float(x))
+    return expected_output, output
+
+
 class Test_deg2rad(unittest.TestCase):
     def test_valid_1(self):
-        with Popen(["./deg2rad.exe", "10"], stdout=PIPE) as p:
-            output = float(p.stdout.read().decode("utf-8"))
-            expected_output = math.radians(10)
-            self.assertAlmostEqual(expected_output, output, 3)
+        expected_output, output = executor("10")
+        self.assertAlmostEqual(expected_output, output, 3)
 
     def test_valid_2(self):
-        with Popen(["./deg2rad.exe", "57.2958"], stdout=PIPE) as p:
-            output = float(p.stdout.read().decode("utf-8"))
-            expected_output = math.radians(57.2958)
-            self.assertAlmostEqual(expected_output, output, 3)
+        expected_output, output = executor("57.2958")
+        self.assertAlmostEqual(expected_output, output, 3)
 
     def test_valid_3(self):
-        with Popen(["./deg2rad.exe", "-45"], stdout=PIPE) as p:
-            output = float(p.stdout.read().decode("utf-8"))
-            expected_output = math.radians(-45)
-            self.assertAlmostEqual(expected_output, output, 3)
+        expected_output, output = executor("-45")
+        self.assertAlmostEqual(expected_output, output, 3)
 
     def test_valid_4(self):
-        with Popen(["./deg2rad.exe", "0"], stdout=PIPE) as p:
-            output = float(p.stdout.read().decode("utf-8"))
-            expected_output = math.radians(0)
-            self.assertAlmostEqual(expected_output, output, 3)
+        expected_output, output = executor("0")
+        self.assertAlmostEqual(expected_output, output, 3)
 
     def test_valid_5(self):
-        with Popen(["./deg2rad.exe", "10000"], stdout=PIPE) as p:
-            output = float(p.stdout.read().decode("utf-8"))
-            expected_output = math.radians(10000)
-            self.assertAlmostEqual(expected_output, output, 3)
+        expected_output, output = executor("10000")
+        self.assertAlmostEqual(expected_output, output, 3)
